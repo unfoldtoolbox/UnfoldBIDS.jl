cf7c1442e7adc28ec10280420fffca41325337d9
ReneSkukies <57703446+ReneSkukies@users.noreply.github.com>
2024-01-18 15:15:45 +0100

    Rebase dev off of main

--- src/utils.jl
+++ src/utils.jl
@@ -13,40 +13,41 @@ extractData (function) - specify the function that translate the MNE Raw object 
 function runUnfold(dataDF, eventsDF, bfDict; eventcolumn="event",removeTimeexpandedXs=true, extractData = rawToData,kwargs...)
 	subjects = unique(dataDF.subject)
 
-	resultsDF = DataFrame()
+    resultsDF = DataFrame()
 
-	for sub in subjects
+    for sub in subjects
 
-		# Get current subject
-		raw = @subset(dataDF, :subject .== sub).data
-		
-		tmpEvents = @subset(eventsDF, :subject .== sub)
+        # Get current subject
+        raw = @subset(dataDF, :subject .== sub).data
 
-		tmpData = extractData(raw[1],tmpEvents;kwargs...)
-		
+        tmpEvents = @subset(eventsDF, :subject .== sub)
 
-		# Fit Model
-		m = fit(UnfoldModel,bfDict,tmpEvents,tmpData; eventcolumn=eventcolumn);
-
-		if removeTimeexpandedXs && (m isa UnfoldLinearModelContinuousTime || m isa UnfoldLinearModelContinuousTime)
-			m = typeof(m)(m.design, Unfold.DesignMatrix(designmatrix(m).formulas, missing, designmatrix(m).events), m.modelfit)
-		end
-		results = DataFrame(:subject => sub, :model => m)
-		
-		append!(resultsDF, results)
+        tmpData = extractData(raw[1], tmpEvents; kwargs...)
 
 
-	end
-	return resultsDF
+        # Fit Model
+        m = fit(UnfoldModel, bfDict, tmpEvents, tmpData; eventcolumn=eventcolumn)
+
+        if removeTimeexpandedXs && (m isa UnfoldLinearModelContinuousTime || m isa UnfoldLinearModelContinuousTime)
+            m = typeof(m)(m.design, Unfold.DesignMatrix(designmatrix(m).formulas, missing, designmatrix(m).events), m.modelfit)
+        end
+        results = DataFrame(:subject => sub, :model => m)
+
+        append!(resultsDF, results)
+
+
+    end
+    return resultsDF
 end
 
 # Function to run Preprocessing functions on data
-function rawToData(raw,tmpEvents;channels::AbstractVector{<:Union{String, Integer}}=[])
-	return pyconvert(Array,raw.get_data(picks=pylist(channels),units="uV"))
+function rawToData(raw, tmpEvents; channels::AbstractVector{<:Union{String,Integer}}=[])
+    return pyconvert(Array, raw.get_data(picks=pylist(channels), units="uV"))
 end
 
 # Calculate Grand average; this is likely a TODO
 # Commented this out for now as this might go into UnfoldStats; R.S. 18/01/24
+
 #=
 function calculateGA(resultsDF; channels=:false)
 	GA = @chain resultsDF begin
@@ -58,7 +59,7 @@ function calculateGA(resultsDF; channels=:false)
 		@by([:basisname,:coefname,:time, :channel], :estimate = mean(estimate))
 	end
 
-    
+
 end
 =#
 
@@ -66,7 +67,7 @@ end
 # Function to run unfold on epoched data
 function runUnfold(DataDF, EventsDF, formula, sfreq, τ = (-0.3,1.); channels::Union{Nothing, String, Integer}=nothing)
 
-	
+
 	# we have multi channel support
 	# data_r = reshape(data,(1,:))
 	# cut the data into epochs
@@ -90,7 +91,7 @@ function runUnfold(DataDF, EventsDF, formula, sfreq, τ = (-0.3,1.); channels::U
 
 		# Cut data into epochs
 		data_epochs,times = Unfold.epoch(data=tmpData,tbl=tmpEvents,τ=τ,sfreq=sfreq);
-		
+
 		# Fit Model
 		m = fit(UnfoldModel,formula,tmpEvents,data_epochs,times);
 		results = coeftable(m)
