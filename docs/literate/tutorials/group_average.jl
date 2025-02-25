a976a40ca453a91eb45d59d482ed7c34e3b5e455
René Skukies <57703446+ReneSkukies@users.noreply.github.com>
2025-02-25 13:23:56 +0100

    Create group_average.jl

---
+++ docs/literate/tutorials/group_average.jl
@@ -0,0 +1,27 @@
+# # Calculate group averages
+
+using UnfoldBIDS
+using Unfold
+using LazyArtifacts
+using Main: @artifact_str # this is a workaround for Artifacts used in docs; locally you would `using LazyArtifacts`
+
+# Load data
+sample_data_path = artifact"sample_BIDS"
+layout_df = bids_layout(sample_data_path, derivatives=false)
+data_df = load_bids_eeg_data(layout_df)
+
+# Calculate results
+basisfunction = firbasis(τ=(-0.2,.8),sfreq=1024)
+f  = @formula 0~1
+bfDict = ["stimulus"=>(f,basisfunction)]
+UnfoldBIDS.rename_to_latency(data_df, :sample) # Unfold expects a :latency collumn in your events; if your event latency is named differently you can use this function as remedy
+
+resultsAll = run_unfold(data_df, bfDict; eventcolumn="trial_type");
+
+# Transform data into tidier format
+ct = bids_coeftable(resultsAll)
+tidy = unpack_results(ct)
+@show tidy
+
+# Calculate average over subjects
+stim_mean_df = combine(groupby(tidy, [:time, :trial_type]), :yhat => mean)
