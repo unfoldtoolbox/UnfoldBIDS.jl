c3e1080c8720d9337b794d25ca46940be2e9b209
René Skukies <science@reneskukies.de>
2025-12-17 13:50:44 +0100

    Update run_unfold docstring

--- src/utils.jl
+++ src/utils.jl
@@ -20,8 +20,8 @@ Run Unfold analysis on all data in data_df. Needs to have PyMNE.jl loaded!
 
 - `remove_time_expanded_Xs::Bool = true`\\
    Removes the timeexpanded designmatrix which significantly reduces the memory-consumption. This Xs is rarely needed, but can be recovered (look into the Unfold.load function)
-- `extract_data::function = raw_to_data`\\
-   Specify the function that translate the MNE Raw object to an data array. Default is `raw_to_data` which uses `get_data` and allows to pick `channels` - see @Ref(`raw_to_data`). The optional kw- arguments (e.g. channels) need to be specified directly in the `run_unfold` function as kw-args
+- `extract_data::function = nothing`\\
+   Specify the function to extract the data to a data array. Falls back to `raw_to_data` which uses `get_data` and allows to pick `channels` if no function is povided AND PyMNE is loaded - see @Ref(`raw_to_data`).
 - `verbose::Bool = true)`\\
    Show ProgressBar or not.
 - `kwargs...`\\
