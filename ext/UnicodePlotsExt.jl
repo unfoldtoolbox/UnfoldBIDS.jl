ef452297eefe71355c6427e4802080b88ef3d143
René Skukies <science@reneskukies.de>
2026-07-29 11:43:29 +0200

    Fix inspect_events

--- ext/UnicodePlotsExt.jl
+++ ext/UnicodePlotsExt.jl
@@ -7,16 +7,20 @@ using Printf
 
 
 """
-    inspect_event(data_df::DataFrame, event_name::Symbol; subject::Union{String, Int}= "all")
-    Inspect an event in the events DataFrame by plotting a unicode histogram and providing summary statistics.
+    inspect_events(data_df::DataFrame, event_collumn::Symbol; event_name::Union= nothing, subject::Union{String, Int}= "all")
+    Inspect an event in the events DataFrame by plotting a unicode histogram and providing summary statistics.\\
+    NOTE: At this point in time, `inspect_events()` only allows to inspect numeric events such as duration or reaction time. Categorical events such as trial_type will be implemented in the future.
     
     ## Arguments
     - `data_df::DataFrame`\\
        DataFrame  containing all subjects and their events. Output of [`load_bids_eeg_data`](@ref)\\
-    - `event_name::Symbol`\\
-       The name of the event to inspect (as found in the :event collumn of events_df).\\
+    - `event_collumn::Symbol`\\
+       The name of the event collumn to inspect (as found in the :events DataFrame of data_df).\\
 
     ## Keywords
+    - `event_name = nothing`\\
+       NOTE: This is a future feature. \\
+       Specify an event name to inspect only that event. Default is `nothing` to inspect all events in the specified collumn.\\
     - `subject::Union{String, Int} = "all"`\\
        Specify a subject to inspect only its events. Default is "all" to inspect all subjects.
     
@@ -24,23 +28,29 @@ using Printf
 
 function inspect_events(
     data_df::DataFrame,
-    event_name::Symbol;
+    event_collumn::Symbol;
+    event_name = nothing,
     subject::Union{String,Int} = "all",
 )
 
     # Extract event of interest`
     if subject != "all"
         @assert subject ∈ data_df.subject "Subject $(subject) not found in DataFrame."
-        events_df = @rsubset(data_df, :subject .== string(subject)).events
+        events_df = UnfoldBIDS.@rsubset(data_df, :subject .== subject)[!, :events][1]
     else
         events_df = UnfoldBIDS.unpack_events(data_df)
     end
 
-    @assert names(events_df) ∋ event_name "Event $(event_name) not found in events DataFrame."
+    @assert names(events_df) ∋ String(event_collumn) "Event $(event_collumn) not found in events DataFrame."
 
     # Extract event of interest
-    d = events_df[:, event_name]
-    name = String(event_name)
+    if true #isnothing(event_name); TODO future implementation
+        d = events_df[:, event_collumn]
+    else
+        @assert event_name ∈ events_df[:, event_collumn] "Event $(event_name) not found in event collumn $(event_collumn)."
+        d = @rsubset(events_df, event_collumn .== event_name)[:, event_collumn]
+    end
+    name = String(event_collumn)
 
 
     # Function to plot unicode histogram
